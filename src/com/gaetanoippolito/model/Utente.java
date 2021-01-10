41bf335c3029d7c59688a892cd3f6f06e65f9aa1
BertoBraghequadre <gaetano.ippolito98@gmail.com>
2021-01-10 18:47:16 +0100

    Aggiunti commenti a "Utente"

--- src/com/gaetanoippolito/model/Utente.java
+++ src/com/gaetanoippolito/model/Utente.java
@@ -3,19 +3,26 @@ package com.gaetanoippolito.model;
 import java.util.Objects;
 
 /**
- * La classe "Utente" rappresenta l'astrazione di ogni singola persona che ha la possibilità di loggare
+ * La classe "Utente" rappresenta l'astrazione di ogni singola persona che ha la possibilità di loggare e registrarsi
  * all'interno dell'applicazione.
  */
-
 public abstract class Utente {
-    // Attributi
+    ///////////////////////////////// VARIABILI DI ISTANZA /////////////////////////////////
     private String username;
     private String password;
     private String nome;
     private String cognome;
     private String email;
 
-    // Costruttore
+    //////////////////////////////////// COSTRUTTORE ////////////////////////////////////
+    /**
+     * Costruttore della classe Utente
+     * @param username Rappresenta l'username dell'utente
+     * @param password Rappresenta la password dell'utente
+     * @param nome Rappresenta il nome dell'utente
+     * @param cognome Rappresenta il cognome dell'utente
+     * @param email Rappresenta l'email dell'utente
+     */
     public Utente(String username, String password, String nome, String cognome, String email){
         this.username = username;
         this.password = password;
@@ -23,48 +30,98 @@ public abstract class Utente {
         this.cognome = cognome;
         this.email = email;
     }
+    ///////////////////////////////////// GETTER /////////////////////////////////////
 
-    // Setters e getters
+    /**
+     * Metodo che ritorna l'username dell'utente
+     * @return Ritorna l'username dell'utente
+     */
     public String getUsername() {
         return this.username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
+    /**
+     * Metodo che ritorna la password dell'utente
+     * @return Ritorna la password dell'utente
+     */
     public String getPassword() {
         return this.password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
+    /**
+     * Metodo che ritorna il nome dell'utente
+     * @return Ritorna il nome dell'utente
+     */
     public String getNome() {
         return this.nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
+    /**
+     * Metodo che ritorna il cognome dell'utente
+     * @return Ritorna il cognome dell'utente
+     */
     public String getCognome() {
         return this.cognome;
     }
 
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
-    }
-
+    /**
+     * Metodo che ritorna l'email dell'utente
+     * @return Ritorna l'email dell'utente
+     */
     public String getEmail() {
         return this.email;
     }
 
+    ///////////////////////////////////// SETTER /////////////////////////////////////
+
+    /**
+     * Metodo che setta la variabile di istanza Username
+     * @param username Rappresenta il valore da settare all'interno della variabile di istanza Username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Metodo che setta la variabile di istanza Password
+     * @param password Rappresenta il valore da settare all'interno della variabile di istanza Password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Metodo che setta la variabile di istanza Nome
+     * @param nome Rappresenta il valore da settare all'interno della variabile di istanza Nome
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * Metodo che setta la variabile di istanza Cognome
+     * @param cognome Rappresenta il valore da settare all'interno della variabile di istanza Cognome
+     */
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    /**
+     * Metodo che setta la variabile di istanza Email
+     * @param email Rappresenta il valore da settare all'interno della variabile di istanza Email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    ////////////////////////////////////// METODI //////////////////////////////////////
+
+    /**
+     * Il metodo equals() che viene ereditato dalla classe Object. Serve per confrontare due oggetti, dove
+     * restituisce true solo se si tratta di due riferimenti allo stesso oggetto.
+     * @param o Rappresenta l'oggetto da paragonare.
+     * @return Ritorna true se i due oggetti sono uguali
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -73,11 +130,19 @@ public abstract class Utente {
         return Objects.equals(username, utente.username) && Objects.equals(password, utente.password);
     }
 
+    /**
+     * Restituisce un valore hash per un oggetto.
+     * @return Rappresenta il valore hash per l'oggetto.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(username, password);
     }
 
+    /**
+     * Restituisce una stringa con tutte le informazioni di un'istanza dell'Azienda.
+     * @return Ritorna una stringa personalizzata con le informazioni di un'istanza dell'Azienda.
+     */
     @Override
     public String toString(){
         return String.format("Username: %s\n" +
