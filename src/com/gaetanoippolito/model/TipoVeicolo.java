6e3c92bdca50066f044fc0d18b29b9de3c512201
BertoBraghequadre <gaetano.ippolito98@gmail.com>
2021-01-10 18:39:14 +0100

    Aggiunti commenti a "TipoVeicolo"

--- src/com/gaetanoippolito/model/TipoVeicolo.java
+++ src/com/gaetanoippolito/model/TipoVeicolo.java
@@ -2,9 +2,19 @@ package com.gaetanoippolito.model;
 
 import java.util.Random;
 
+/**
+ * Enum contenente le varie tipologie di un Veicolo
+ */
 public enum TipoVeicolo {
+    ////////////////////////////////////// CASE //////////////////////////////////////
     CAMION, FURGONE, AUTO;
 
+    ////////////////////////////////////// METODI //////////////////////////////////////
+
+    /**
+     * Metodo statico che ritorna un case randomico dell'enum
+     * @return Ritorna un case dell'enum
+     */
     public static TipoVeicolo getRandomTipoVeicolo(){
         Random random = new Random();
         int randomMax = 2;
