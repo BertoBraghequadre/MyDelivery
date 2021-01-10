3eaeb0ba54981a19a21e8aafad0c87c5c4442a94
BertoBraghequadre <gaetano.ippolito98@gmail.com>
2021-01-10 19:08:40 +0100

    Aggiunti commenti a "Veicolo"

--- src/com/gaetanoippolito/model/Veicolo.java
+++ src/com/gaetanoippolito/model/Veicolo.java
@@ -1,7 +1,5 @@
 package com.gaetanoippolito.model;
 
-import com.gaetanoippolito.model.database.MyDeliveryData;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Objects;
@@ -11,10 +9,11 @@ import java.util.Objects;
  */
 
 public class Veicolo implements Serializable {
+    ///////////////////////////////// VARIABILI DI ISTANZA /////////////////////////////////
+
     // id del "serialVersionUID"
     private static final long serialVersionUID = 2L;
 
-    // Attributi
     /**@see TipoVeicolo*/
     private TipoVeicolo tipoVeicolo;
     private double capienzaContainer;
@@ -23,45 +22,92 @@ public class Veicolo implements Serializable {
     /**@see Pacco*/
     private ArrayList<Pacco> pacchiDepositati;
 
-    // Costruttore
+    //////////////////////////////////// COSTRUTTORE ////////////////////////////////////
+    /**
+     * Costruttore di Veicolo
+     * @param tipoVeicolo Rappresenta la tipologia del veicolo
+     * @param capienzaContainer Rappresenta la capienza del container del veicolo
+     * @param codice Rappresenta il codice del veicolo
+     */
     public Veicolo(TipoVeicolo tipoVeicolo, double capienzaContainer, int codice){
         this.tipoVeicolo = tipoVeicolo;
         this.capienzaContainer = capienzaContainer;
         this.codice = codice;
     }
 
+    /**
+     * Costruttore di Veicolo associato ad un'azienda.
+     * @param tipoVeicolo Rappresenta la tipologia del veicolo
+     * @param capienzaContainer Rappresenta la capienza del container del veicolo
+     * @param codice Rappresenta il codice del veicolo
+     * @param aziendaAssociata Rappresenta l'azienda associata al veicolo
+     * @see Azienda
+     */
     public Veicolo(TipoVeicolo tipoVeicolo, double capienzaContainer, int codice, String aziendaAssociata){
         this(tipoVeicolo, capienzaContainer, codice);
         this.aziendaAssociata = aziendaAssociata;
         this.pacchiDepositati = new ArrayList<>();
     }
 
-    // Getter e Setter
+    ///////////////////////////////////// GETTER /////////////////////////////////////
+    /**
+     * Metodo che restituisce la tipologia del veicolo
+     * @return Ritorna la tipologia del veicolo
+     */
     public TipoVeicolo getTipoVeicolo() {
         return this.tipoVeicolo;
     }
 
+    /**
+     * Metodo che restituisce la capienza del container del veicolo
+     * @return Ritorna la capienza del container del veicolo
+     */
     public double getCapienzaContainer() {
         return this.capienzaContainer;
     }
 
+    /**
+     * Metodo che restituisce il codice del veicolo
+     * @return Ritorna il codice del veicolo
+     */
     public int getCodice() {
         return this.codice;
     }
 
+    /**
+     * Metodo che restituisce la lista di pacchi contenuti nel veicolo
+     * @return Ritorna il codice del veicolo
+     */
     public ArrayList<Pacco> getPacchiDepositati() {
         return this.pacchiDepositati;
     }
 
-    public void setPacchiDepositati(ArrayList<Pacco> pacchiDepositati) {
-        this.pacchiDepositati = pacchiDepositati;
-    }
-
+    /**
+     * Metodo che restituisce il nome dell'azienda associata al veicolo
+     * @return Ritorna il nome dell'azienda associata al veicolo
+     */
     public String getAziendaAssociata(){
         return this.aziendaAssociata;
     }
 
-    // Metodi
+    ///////////////////////////////////// SETTER /////////////////////////////////////
+
+    /**
+     * Metodo che setta la ArrayList di pacchi con uno dato in input
+     * @param pacchiDepositati Rappresenta un ArrayList di pacchi
+     */
+    public void setPacchiDepositati(ArrayList<Pacco> pacchiDepositati) {
+        this.pacchiDepositati = pacchiDepositati;
+    }
+
+    ////////////////////////////////////// METODI //////////////////////////////////////
+
+    /**
+     * Il metodo equals() che viene ereditato dalla classe Object. Serve per confrontare due oggetti, dove
+     * restituisce true solo se si tratta di due riferimenti allo stesso oggetto.
+     * @param o Rappresenta l'oggetto da paragonare.
+     * @return Ritorna true se i due oggetti sono uguali
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -70,11 +116,19 @@ public class Veicolo implements Serializable {
         return Objects.equals(codice, veicolo.codice);
     }
 
+    /**
+     * Restituisce un valore hash per un oggetto.
+     * @return Rappresenta il valore hash per l'oggetto.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(codice);
     }
 
+    /**
+     * Restituisce una stringa con tutte le informazioni di un'istanza dell'Azienda.
+     * @return Ritorna una stringa personalizzata con le informazioni di un'istanza dell'Azienda.
+     */
     @Override
     public String toString() {
         return String.format("Tipo veicolo: %s - " +
