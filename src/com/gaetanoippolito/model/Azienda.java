715c91257f478499fd469122bd99521c03fbaa3d
BertoBraghequadre <gaetano.ippolito98@gmail.com>
2021-01-10 18:33:21 +0100

    Aggiungi commenti a "Azienda"

--- src/com/gaetanoippolito/model/Azienda.java
+++ src/com/gaetanoippolito/model/Azienda.java
@@ -1,61 +1,90 @@
 package com.gaetanoippolito.model;
 
 import com.gaetanoippolito.model.database.MyDeliveryData;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Random;
 
+/**
+ * Questa Classe rappresenta l'astrazione dell'azienda con cui il cliente interagisce al momento della creazione
+ * di un ordine.
+ */
 public class Azienda implements Serializable {
+    ///////////////////////////////// VARIABILI DI ISTANZA /////////////////////////////////
     // id del "serialVersionUID" di default
     private static final long serialVersionUID = 1L;
 
-    // Attributi
     private String nomeAzienda;
     private String partitaIVA;
+    /**@see Veicolo*/
     private ArrayList<Veicolo> veicoli;
     // TODO: Aggiungere i Corrieri
 
-    // Costruttori
+    //////////////////////////////////// COSTRUTTORE ////////////////////////////////////
+    /**
+     * Costruttore completo che setta le variabili di istanza.
+     * @param nomeAzienda Rappresenta il nome dell'azienda
+     * @param partitaIVA Rappresenta la partita IVA dell'azienda
+     * @param veicoli Rappresenta la lista di veicoli presenti nell'azienda
+     */
     public Azienda(String nomeAzienda, String partitaIVA, ArrayList<Veicolo> veicoli){
         this.nomeAzienda = nomeAzienda;
         this.partitaIVA = partitaIVA;
-        this.veicoli = associaVeicoli(nomeAzienda);
+
+        if(veicoli.size() == 0){
+            this.veicoli = associaVeicoli(nomeAzienda);
+        }
+        else{
+            this.veicoli = veicoli;
+        }
     }
 
-    public Azienda(String nome, String partitaIVA){
-        this(nome, partitaIVA, new ArrayList<>());
+    /**
+     * Costruttore da utilizzare nel caso in cui non si ha una lista di Veicoli o Corrieri da passare
+     * @param nomeAzienda Rappresenta il nome dell'azienda
+     * @param partitaIVA Rappresenta la partita IVA dell'azienda
+     */
+    public Azienda(String nomeAzienda, String partitaIVA){
+        this(nomeAzienda, partitaIVA, new ArrayList<>());
     }
 
-    // Setter e Getter
+    ///////////////////////////////////// GETTER /////////////////////////////////////
+    /**
+     * Metodo che restituisce il nome dell'azienda
+     * @return Ritorna il nome dell'azienda
+     */
     public String getNome() {
         return this.nomeAzienda;
     }
 
-    public void setNome(String nome) {
-        this.nomeAzienda = nome;
-    }
-
+    /**
+     * Metodo che restituisce la partita IVA dell'azienda
+     * @return Ritorna la partita IVA dell'azienda
+     */
     public String getPartitaIVA() {
         return this.partitaIVA;
     }
 
-    public void setPartitaIVA(String partitaIVA) {
-        this.partitaIVA = partitaIVA;
-    }
-
+    /**
+     * Metodo che restituisce la lista dei veicoli dell'azienda
+     * @return Ritorna la lista dei veicoli dell'azienda
+     * @see Veicolo
+     */
     public ArrayList<Veicolo> getVeicoli() {
         return this.veicoli;
     }
 
-    public void setVeicoli(ArrayList<Veicolo> veicoli) {
-        this.veicoli = veicoli;
-    }
-
-    // Metodi
+    ////////////////////////////////////// METODI //////////////////////////////////////
+    /**
+     * Metodo privato per creare dei veicoli da associare all'azienda nel momento della creazione di un'azienda.
+     * @param nomeAzienda Rappresenta il nome dell'azienda da passare al costruttore del Veicolo
+     * @return Ritorna un ArrayList di veicoli generati randomicamente.
+     */
     private ArrayList<Veicolo> associaVeicoli(String nomeAzienda){
+        /**@see Random*/
         Random random = new Random();
+        /**@see Veicolo*/
         Veicolo veicoloAssociato;
         ArrayList<Veicolo> veicoliDiAzienda = new ArrayList<>();
         TipoVeicolo tipoVeicolo;
@@ -90,6 +119,12 @@ public class Azienda implements Serializable {
         return veicoliDiAzienda;
     }
 
+    /**
+     * Il metodo equals() che viene ereditato dalla classe Object. Serve per confrontare due oggetti, dove
+     * restituisce true solo se si tratta di due riferimenti allo stesso oggetto.
+     * @param o Rappresenta l'oggetto da paragonare.
+     * @return Ritorna true se i due oggetti sono uguali
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -98,11 +133,19 @@ public class Azienda implements Serializable {
         return Objects.equals(partitaIVA, azienda.partitaIVA);
     }
 
+    /**
+     * Restituisce un valore hash per un oggetto.
+     * @return Rappresenta il valore hash per l'oggetto.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(partitaIVA);
     }
 
+    /**
+     * Restituisce una stringa con tutte le informazioni di un'istanza dell'Azienda.
+     * @return Ritorna una stringa personalizzata con le informazioni di un'istanza dell'Azienda.
+     */
     @Override
     public String toString() {
         return String.format("Nome azienda: %s - Partita IVA: %s - Veicoli: %s",
