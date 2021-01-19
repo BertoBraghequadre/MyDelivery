20f3e6ce685a781a1b5bede72d1c1014146eedfc
BertoBraghequadre <gaetano.ippolito98@gmail.com>
2021-01-19 18:17:37 +0100

    Aggiunti commenti per "ObserverDestinatario"

--- src/com/gaetanoippolito/model/observerPattern/ObserverDestinatario.java
+++ src/com/gaetanoippolito/model/observerPattern/ObserverDestinatario.java
@@ -1,6 +1,14 @@
 package com.gaetanoippolito.model.observerPattern;
 
-
+/**
+ * Questa classe rappresenta l'interfaccia dell'observer. Espone l’interfaccia che consente di aggiornare gli
+ * osservatori in caso di cambio di stato del soggetto osservato.
+ */
 public interface ObserverDestinatario {
+    /**
+     * Questo è un metodo sfruttato delle classi observable in cui si aggiorna lo stato di tutti gli observer
+     * che osservano.
+     * @param statoAggiornato Rappresenta lo stato aggiornato.
+     */
     void update(Stato statoAggiornato);
 }
