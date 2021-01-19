b38228a1528383235a20cb8f2adb8423bcc7c644
BertoBraghequadre <gaetano.ippolito98@gmail.com>
2021-01-19 12:20:12 +0100

    Aggiunti commenti per "RegisterController"

--- src/com/gaetanoippolito/controller/dialog/RegisterController.java
+++ src/com/gaetanoippolito/controller/dialog/RegisterController.java
@@ -8,25 +8,49 @@ import javafx.beans.binding.BooleanExpression;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+/**
+ * Questa classe rappresenta il controller del Dialog per la registrazione di un Cliente.
+ */
+
 public class RegisterController {
+    /**@see TextField*/
     @FXML
     private TextField nomeTextField;
+
+    /**@see TextField*/
     @FXML
     private TextField cognomeTextField;
+
+    /**@see TextField*/
     @FXML
     private TextField usernameTextField;
+
+    /**@see TextField*/
     @FXML
     private TextField passwordTextField;
+
+    /**@see TextField*/
     @FXML
     private TextField emailTextField;
+
+    /**@see TextField*/
     @FXML
     private TextField indirizzoTextField;
+
+    /**@see TextField*/
     @FXML
     private TextField cfTextField;
+
+    /**@see TextField*/
     @FXML
     private TextField numeroDiTelefonoTextField;
 
-
+    /**
+     * Questo metodo aggiunge un cliente se e solo se il cliente che si vuole aggiungere non sia già presente
+     * all'interno di MyDeliveryData.
+     * @return Ritorna una Stringa che verrà utilizzata dal Label del login Controller per segnalare se il cliente
+     *         è stato aggiunto oppure no.
+     */
     public String aggiungiNuovoAccount(){
         String nome = this.nomeTextField.getText().trim();
         String cognome = this.cognomeTextField.getText().trim();
@@ -46,6 +70,10 @@ public class RegisterController {
         }
     }
 
+    /**
+     * Questo metodo serve a disabilitare l'OK button se i textField sono vuoti, in maniera dinamica
+     * @return ritorna una espressione booleana osservabile
+     */
     // Una BooleanExpression ritorna una espressione booleana osservabile
     public BooleanExpression disabilitaTastoOK(){
         // "createBooleanBinding" accetta una funzione lamba che rappresenta la nostra espressione booleana
