b41638ae2459c34a1f3aef1aa36c01c6de9b444e
BertoBraghequadre <gaetano.ippolito98@gmail.com>
2021-01-19 12:14:04 +0100

    Aggiunti commenti per "NextFitDialogController"

--- src/com/gaetanoippolito/controller/dialog/NextFitDialogController.java
+++ src/com/gaetanoippolito/controller/dialog/NextFitDialogController.java
@@ -7,25 +7,42 @@ import javafx.beans.binding.BooleanExpression;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 
+/**
+ * Questa classe rappresenta il controller del dialog che permette l'utilizzo dell'algortimo NextFit.
+ */
+
 public class NextFitDialogController {
+    ///////////////////////////////// VARIABILI DI ISTANZA /////////////////////////////////
+    /**@see ChoiceBox*/
     @FXML
     private ChoiceBox<Azienda> aziendeChoiceBox;
 
+    ////////////////////////////////////// METODI //////////////////////////////////////
+    /**
+     * Metodo overridato che viene triggerato nel momento in cui viene inizializzata la view. Questo metodo inoltre
+     * setta gli items della choiceBox di aziende.
+     */
     @FXML
     public void initialize(){
         this.aziendeChoiceBox.setItems(MyDeliveryData.getInstance().getAziende());
     }
 
+    /**
+     * Questo metodo ritorna l'Azienda selelezionata dall'utente con cui processare l'algoritmo NextFit
+     * @return Ritorna l'azienda selezionata dall'utente
+     */
     public Azienda aziendaSelezionata(){
         return aziendeChoiceBox.getValue();
     }
 
+    /**
+     * Questo metodo serve a disabilitare l'OK button se l'azienda non è stata selezionata in maniera dinamica
+     * @return ritorna una espressione booleana osservabile
+     */
     // Una BooleanExpression ritorna una espressione booleana osservabile
     public BooleanExpression disabilitaTastoOK(){
         // "createBooleanBinding" accetta una funzione lamba che rappresenta la nostra espressione booleana
         // e le proprietà osservabili.
-        // Semplificando: Ritorna una espressione booleana ad ogni cambiamento del testo dei vari TextField
-        // "textProperty" ritorna il testo corrente
         return Bindings.createBooleanBinding(() -> aziendeChoiceBox.getValue() == null, aziendeChoiceBox.valueProperty());
     }
 }
